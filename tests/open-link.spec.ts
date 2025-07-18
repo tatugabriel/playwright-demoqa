01cf6fcb1ecc3b4cfea2e384ade4e8851cf163f0
tatugabriel <tatugabriel@yahoo.ro>
2025-07-18 21:32:29 +0300

    update: add new test cases and assertions

--- tests/open-link.spec.ts
+++ tests/open-link.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('open base URL', async ({ page, baseURL }) => {
   
   await page.goto(baseURL!); // use baseURL provided by config
-  await page.pause();
+  await expect(page.locator('body')).toContainText('Forms');
 });
--- tests/type_name.spec.ts
+++ tests/type_name.spec.ts
@@ -5,9 +5,13 @@ test('test', async ({ page, baseURL }) => {   // <--- add baseURL here!
   await page.locator('div').filter({ hasText: /^Forms$/ }).first().click();
   await page.getByRole('listitem').click();
   await page.getByRole('textbox', { name: 'First Name' }).fill('XXXXX');
+  await page.getByRole('textbox', { name: 'Last Name' }).fill('YYYY');
+
 
    // Assert the textbox contains 'XXXXX'
   await expect(page.getByRole('textbox', { name: 'First Name' })).toHaveValue('XXXXX');
-
+  await expect(page.getByRole('textbox', { name: 'Last Name' })).toHaveValue('YYYY');
   await page.pause(); // optional for inspection/debugging
+
+
 });
